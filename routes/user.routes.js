55c210e6f7015671f49cbec360b18208fa8f370f
antdamj <antonio.damjanovic.8@gmail.com>
2020-11-10 01:19:40 +0100

    dodana promjena lozinke

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,28 +1,59 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/UserModel');
 
 //prikaz podataka o korisniku (podaci o korisniku, adrese, narudžbe)
 router.get('/', function (req, res, next) {
-
     (async () => {
-
-        //ako korisnik nije logiran, redirekcija na osnovnu stranicu
         if (req.session.user === undefined) {
             res.redirect('/')
             return
         }
 
-        //dobavi narudžbe korisnika
-        //let orders = await Order.fetchByUser(req.session.user)
-
         res.render('user', {
             title: 'Korisnički profil',
             user: req.session.user,
-            //orders: orders,
-            linkActive: 'user'
+            linkActive: 'user',
+            err: undefined
         });
     })()
+});
+
+router.post('/', async (req, res, next) => {
+    //provjeri istovjetnost unesenenih zaporki
+
+    if (req.body.password1 != req.body.password2) {
+        res.render('User', {
+            title: 'Korisnički profil',
+            linkActive: 'user',
+            user: req.session.user,
+            err: "Lozinke se ne podudaraju."
+        })
+        return
+    }
+    //ako je nova lozinka jednaka trenutnoj, javi grešku
+    if (req.body.password == req.body.password1) {
+        res.render('User', {
+            title: 'Korisnički profil',
+            linkActive: 'user',
+            user: req.session.user,
+            err: "Nova lozinka jednaka je trenutnoj lozinci."
+        })
+        return
+    }
+
+    if (req.body.password !== req.session.user.lozinka) {
+        res.render('User', {
+            title: 'Korisnički profil',
+            linkActive: 'user',
+            user: req.session.user,
+            err: "Neispravna trenutna lozinka."
+        })
+        return
+    }
+    User.changePassword(req.session.user.korisnickoime, req.body.password1);
+    req.session.user.lozinka = req.body.password1;
+    res.redirect('/');
 });
 
 module.exports = router;
