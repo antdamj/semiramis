9ed3ecccf7b381403c363f470361c5147f7733bc
D_Sekula <ds51952@fer.hr>
2021-01-03 18:46:54 +0100

    Nadogradena search stranica, potrebno SQL realizirati do kraja i frontend

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,14 +3,54 @@ const db = require('../db');
 const router = express.Router();
 
 router.get('/', async function (req, res, next) {
-    let carsDb=await db.query('SELECT * FROM vozilo');
-    console.log(carsDb.rows);
+   
+    let poslovnicaDb= await db.query('SELECT * FROM poslovnica');
+
+    /*
+    let datum_primitka = new Date();
+    let datum_povratka = new Date(datum_primitka);
+    datum_povratka.setDate(datum_povratka.getDate() + 1)
+    
+    let day=datum_primitka.getDate();
+    let month=datum_primitka.getMonth()+1;
+    let year=datum_primitka.getFullYear();
+    
+    
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    let maxDate = year + '-' + month + '-' + day; 
+    let newDate=new Date(maxDate);
+    */
+
     res.render('search', {
         title: 'Pretraga',
         user: req.session.user,
         linkActive: 'search',      //TODO 
-        cars: carsDb.rows
+        locations: poslovnicaDb.rows,
+        cars: "",
+        isHidden:false
     });
 });
+
+router.post('/',async function(req,res,next){
+    let carsDb=await db.query('SELECT * FROM vozilo');
+    let poslovnicaDb= await db.query('SELECT * FROM poslovnica');
+
+
+    console.log(req.body);
+    let date=req.body.datum_primitka+" "+req.body.vrijeme_primitka;
+    console.log(date);
+    res.render('search', {
+        title: 'Pretraga',
+        user: req.session.user,
+        linkActive: 'search',      //TODO 
+        cars: carsDb.rows,
+        locations: poslovnicaDb.rows,
+        isHidden:true
+    });
+
+})
 
 module.exports = router;
